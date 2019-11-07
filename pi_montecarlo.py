266ac82a2a6a7325a75f9be301a5b376ba8559f1
Alain <alaingirardvd@gmail.com>
2019-11-06 19:21:35 -1000

    Argument work with argparse

--- pi_montecarlo.py
+++ pi_montecarlo.py
@@ -1,24 +1,36 @@
 """Calcule pi grace à la méthode de monte Carlo"""
 
 import random
-import sys
+import argparse
 
 
-def main():
+def compute_pi(iteration):
     """Fonction principale"""
-    total = int(sys.argv[1])
-    # TODO catch error
 
     inside = 0
 
-    for _ in range(total):
+    for _ in range(iteration):
         x_coord = random.random()
         y_coord = random.random()
 
         if (x_coord**2 + y_coord**2) < 1:
             inside += 1
 
-    print(inside/total*4)
+    print(inside/iteration*4)
+
+
+def get_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "iteration", help="number of iterations to compute pi", type=int)
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    iteration = get_argument().iteration
+    compute_pi(iteration)
 
 
 if __name__ == "__main__":
