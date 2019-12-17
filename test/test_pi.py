1ea0ea0661a7c3577f7d6ffad4bd8bcc6790817d
Alain <alaingirardvd@gmail.com>
2019-12-17 22:28:06 +1100

    Update test

--- test/test_pi.py
+++ test/test_pi.py
@@ -15,6 +15,6 @@ class TestPi(unittest.TestCase):
         """
         Test the accuracy
         """
-        pi_aprox = compute_pi(500)
+        pi_aprox = compute_pi(500, False)
         self.assertGreater(pi_aprox, 2.5)
         self.assertLess(pi_aprox, 3.5)
